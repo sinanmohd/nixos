467dc80d65f33cd1d5d2515c84c9ad0297151315
sinanmohd <sinan@sinanmohd.com>
2024-02-05 13:20:07 +0530

    lia/users/rohit: initial conf

--- hosts/lia/modules/users.nix
+++ hosts/lia/modules/users.nix
@@ -1,5 +1,10 @@
-{ ... }: {
+{ pkgs, ... }: {
   users.users."rohit" = {
     isNormalUser = true;
+    extraGroups = [ "wheel" ];
+
+    packages = with pkgs; [ git htop ];
+    openssh.authorizedKeys.keys =
+      [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOZcWF1zVyxsCdZ/j+h+RlHZlyhgY2Bky03847bxFNSH rohit@victus" ];
   };
 }
