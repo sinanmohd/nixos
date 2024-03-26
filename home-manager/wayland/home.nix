854039328f9d955ef6b8c47e3bbcec4392a22df2
sinanmohd <sinan@sinanmohd.com>
2024-03-26 21:10:17 +0530

    home-manager/wayland/mako: init

--- home-manager/wayland/home.nix
+++ home-manager/wayland/home.nix
@@ -3,5 +3,6 @@
     ../common/home.nix
     ./modules/foot.nix
     ./modules/zathura.nix
+    ./modules/mako.nix
   ];
 }
---
+++ home-manager/wayland/modules/mako.nix
@@ -0,0 +1,10 @@
+{ ... }: {
+  services.mako = {
+    enable = true;
+    defaultTimeout = 3000;
+
+    borderSize = 2;
+    font = "Sans 13";
+    backgroundColor = "#000000";
+  };
+}
