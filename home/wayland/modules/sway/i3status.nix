364ded5b5ab55becf9e6904bebe2b67f9cfa343a
sinanmohd <sinan@sinanmohd.com>
2024-06-04 10:38:55 +0530

    home/wayland/sway/i3status: init battery and wireless module

--- home/wayland/modules/sway/i3status.nix
+++ home/wayland/modules/sway/i3status.nix
@@ -7,28 +7,55 @@
       output_format = "i3bar";
       separator = "";
       interval = 5;
-      colors = false;
     };
 
     modules = {
       "tztime local" = {
-        position = 30;
+        position = 50;
         settings.format = "󱛡   %a,%e %b  %H:%M";
       };
+
+      "battery all" = {
+        position = 40;
+        settings = {
+          status_chr = "󰂄";
+          status_bat = "󰂀";
+          status_unk = "󰂃";
+          status_full = "󱟨";
+          last_full_capacity = true;
+          low_threshold = 20;
+          threshold_type = "percentage";
+          format_percentage = "%.00f%s";
+          path = "/sys/class/power_supply/BAT%d/uevent";
+          format_down = "";
+          format = "%status %percentage %remaining %consumption";
+        };
+      };
+
       "volume master" = {
-        position = 20;
+        position = 30;
         settings = {
           format = "󰕾  %volume";
           format_muted = "󰖁  %volume";
         };
       };
+
       "memory" = {
-        position = 10;
+        position = 20;
         settings.format = "  %available";
       };
+
       "disk /" = {
-        position = 00;
+        position = 10;
         settings.format = "󰋊   %avail";
+      };
+
+      "wireless _first_" = {
+        position = 00;
+        settings = {
+          format_up = "󰤥  %quality";
+          format_down = "";
+        };
       };
     };
   };
