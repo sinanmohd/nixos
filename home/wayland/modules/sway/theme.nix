851410a9d6393ae2c219983292dca93368294e28
sinanmohd <sinan@sinanmohd.com>
2024-06-06 14:23:43 +0530

    home/wayland/sway/theme: remove rounded corners and dropshadow

--- home/wayland/modules/sway/theme.nix
+++ home/wayland/modules/sway/theme.nix
@@ -3,7 +3,27 @@
 
   gtk = {
     enable = true;
-    gtk3.extraConfig.gtk-application-prefer-dark-theme = 1;
+
+    gtk3 = {
+      extraConfig.gtk-application-prefer-dark-theme = 1;
+      # remove rounded corners and dropshadow
+      # https://github.com/swaywm/sway/wiki/GTK-3-settings-on-Wayland
+      extraCss = ''
+        .titlebar,
+        window {
+                border-radius: 0;
+                box-shadow: none;
+        }
+
+        decoration {
+                box-shadow: none;
+        }
+
+        decoration:backdrop {
+                box-shadow: none;
+        }
+      '';
+    };
   };
 
   home.pointerCursor = {
