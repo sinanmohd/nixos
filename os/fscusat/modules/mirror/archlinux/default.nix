5b8bdcb63cc42945996749c3371d0b639f6e8c21
Harshith reddy <me.harshithreddy@gmail.com>
2024-06-17 09:03:50 +0530

    fscusat/mirror/archlinux: update

--- os/fscusat/modules/mirror/archlinux/default.nix
+++ os/fscusat/modules/mirror/archlinux/default.nix
@@ -1,84 +1,27 @@
 { pkgs, ... }:
 
 let
-  target = "/var/lib/archlinux";
-  lock = "/var/lock/syncrepo.lck";
-  bwlimit = 0;
-  source_url = "rsync://mirror.guillaumea.fr/archlinux/";
-  lastupdate_url = "https://mirror.guillaumea.fr/archlinux/lastupdate";
-
-  rsync_cmd = { src, dest, excludes ? [], extraArgs ? [] }@args:
-    let
-      cmd = pkgs.writeScript "rsync_cmd-${src}" ''
-        rsync -rlptH --safe-links --delete-delay --delay-updates \
-          --timeout=600 --contimeout=60 --no-motd \
-          ${if pkgs.stty then "-h -v --progress" else "--quiet"} \
-          ${if bwlimit > 0 then "--bwlimit=$bwlimit" else ""} \
-          ${lib.concatMapStrings (s: " --exclude='${s}'") excludes} \
-          ${src} ${dest} ${lib.concatMapStrings (s: " '${s}'") extraArgs}
-      '';
-    in
-      pkgs.stdenv.mkDerivation {
-        name = "rsync_cmd-${src}";
-        buildInputs = [ pkgs.rsync pkgs.stty ];
-        buildPhase = ''
-          mkdir -p $out/bin
-          cp ${cmd} $out/bin/rsync_cmd-${src}
-          chmod +x $out/bin/rsync_cmd-${src}
-        '';
-      };
-
+  interval = "6h";
 in
 {
-  environment.systemPackages = [ pkgs.coreutils ];
+  systemd.services.archSyncService = {
+    description = "Run my script every 6 hours";
 
-  rsync = rsync_cmd {
-    src = source_url;
-    dest = target;
-    excludes = [ "*.links.tar.gz*" "/other" "/sources" ];
-  };
-
-  checkAndUpdateMirror = pkgs.writeScriptBin "checkAndUpdateMirror" ''
-    if [[ ! -d "${target}" ]]; then
-      mkdir -p "${target}"
-    fi
-
-    exec 9>"${lock}"
-    flock -n 9 || exit
-
-    # Cleanup any temporary files from old run
-    find "${target}" -name '.~tmp~' -exec rm -r {} +
-
-    if [[ ! -t 1 ]] && [[ -f "$target/lastupdate" ]] && diff -b <(curl -Ls "$lastupdate_url") "$target/lastupdate" >/dev/null; then
-      ${rsync} --exclude='*.links.tar.gz*' --exclude='/other' --exclude='/sources' --exclude='/lastsync' "${source_url}/lastsync" "${target}/lastsync"
-      exit 0
-    fi
-
-    ${rsync}
-  '';
-
-  systemd.services.sync-archlinux-mirror = {
-    description = "Sync Arch Linux Mirror";
     serviceConfig = {
-      ExecStart = "${pkgs.writeScriptBin "sync-archlinux-mirror" ''
-        ${rsync_cmd { src = source_url; dest = target; excludes = [ "*.links.tar.gz*" "/other" "/sources" ]; }}
-      ''}";
-      ExecStartPost = "${pkgs.writeScriptBin "update-lastupdate" ''
-        cp ${target}/lastupdate ${target}/lastupdate.new
-        mv ${target}/lastupdate.new ${target}/lastupdate
-      ''}";
-      Type = "oneshot";
-      User = "root";
-      Group = "root";
+      ExecStart = "${pkgs.bash}/bin/bash ${./sync-repo.sh}";
     };
+
+    wantedBy = [ "multi-user.target" ];
   };
 
-  systemd.timers.sync-archlinux-mirror-timer = {
-    description = "Run Sync Arch Linux Mirror every 6 hours";
-    wantedBy = [ "timers.target" ];
+  systemd.timers.archSyncTimer = {
+    description = "Timer to run my script every 6 hours";
+
     timerConfig = {
-      OnCalendar = "0/6:00:00";
-      Persistent = true;
+      OnUnitActiveSec = interval;
+      Unit = "archSyncService.service";
     };
+
+    wantedBy = [ "timers.target" ];
   };
 }
