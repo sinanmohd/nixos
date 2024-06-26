a8a88c53b353205be0cb9b6cba87700b2dc971a9
sinanmohd <sinan@sinanmohd.com>
2024-06-26 15:05:28 +0530

    home/pc/pass: setup gnupg

--- home/pc/modules/pass.nix
+++ home/pc/modules/pass.nix
@@ -1,10 +1,20 @@
-{ pkgs, ... }: {
-  home.packages = with pkgs; [
-    (pass.withExtensions (exts: [ exts.pass-otp ]))
-  ];
+{ config, pkgs, lib, ... }: let
+  passStore = config.xdg.dataHome + "/pass";
+  GNUPGHome = config.xdg.dataHome + "/gnupg";
+in {
+  home = {
+    file."${GNUPGHome}/gpg-agent.conf".text = ''
+      pinentry-program ${lib.getExe pkgs.pinentry-bemenu}
+    '';
 
-  services.gpg-agent = {
-    enable = true;
-    pinentryPackage = pkgs.pinentry-bemenu;
+    packages = with pkgs; [
+      pinentry-bemenu
+      (pass.withExtensions (exts: [ exts.pass-otp ]))
+    ];
+
+    sessionVariables = {
+      PASSWORD_STORE_DIR = passStore;
+      GNUPGHOME = GNUPGHome;
+    };
   };
 }
