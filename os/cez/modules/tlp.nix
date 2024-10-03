0657599d302afbfa38217409bfd0573b0c818743
sinanmohd <sinan@sinanmohd.com>
2024-10-03 20:44:48 +0530

    cez: enable bluetooth

--- os/cez/modules/tlp.nix
+++ os/cez/modules/tlp.nix
@@ -11,9 +11,6 @@
       CPU_SCALING_GOVERNOR_ON_AC = "performance";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
-      DEVICES_TO_ENABLE_ON_AC = "bluetooth";
-      DEVICES_TO_DISABLE_ON_BAT_NOT_IN_USE = "bluetooth";
-
       CPU_BOOST_ON_AC = 1;
       CPU_BOOST_ON_BAT = 0;
       CPU_HWP_DYN_BOOST_ON_AC = 1;
