ec5fc01b62f0c2fb680e5cb9233b3533f18105b3
sinanmohd <sinan@sinanmohd.com>
2024-10-27 21:41:23 +0530

    home/pc/dev: generate caches for man pages

--- home/pc/modules/dev.nix
+++ home/pc/modules/dev.nix
@@ -1,10 +1,14 @@
 { pkgs, ... }: {
-  programs.man.enable = true;
+  programs.man = {
+    enable = true;
+    generateCaches = true;
+  };
 
   home.packages = with pkgs; [
     git
     sops
 
+    linux-manual
     man-pages
     man-pages-posix
 
