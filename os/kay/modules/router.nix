11c905e7ed26f03c05a72b82cabcfd3e3d0d7599
sinanmohd <sinan@sinanmohd.com>
2024-11-25 08:34:52 +0530

    kay/router: fine tune router advertisement

--- os/kay/modules/router.nix
+++ os/kay/modules/router.nix
@@ -40,7 +40,7 @@ in {
       # https://github.com/NixOS/nixpkgs/blob/e8c38b73aeb218e27163376a2d617e61a2ad9b59/nixos/modules/services/networking/dhcpcd.nix#L13
       # without this dhcpcd will not run, and if we set it to wanInterface,
       # when pppd(ppp0 iface) exit it'll take out wan vlan iface as well
-      lo.useDHCP = true;
+      ${wanInterface}.useDHCP = true;
     };
     firewall = {
       allowedUDPPorts = [ 53 67 ];
@@ -105,11 +105,12 @@ in {
           AdvLinkMTU ${toString wanMTU};
 
           MinRtrAdvInterval 3;
-          MaxRtrAdvInterval 10;
+          MaxRtrAdvInterval 6;
+          AdvDefaultLifetime 60;
 
           prefix ::/64 {
-            AdvPreferredLifetime 60;
-            AdvValidLifetime 120;
+            AdvPreferredLifetime 30;
+            AdvValidLifetime 60;
           };
         };
       '';
