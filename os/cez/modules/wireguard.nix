2e2731380c81a020f7bad594f475a6b4da4465f1
sinanmohd <sinan@sinanmohd.com>
2025-07-30 18:42:37 +0530

    os/cez/wireguard: add bud

--- os/cez/modules/wireguard.nix
+++ os/cez/modules/wireguard.nix
@@ -25,27 +25,45 @@ let
 in
 {
   sops.secrets."misc/wireguard" = { };
-
-  networking.wg-quick.interfaces.${wgIface} = {
-    autostart = false;
-    address = [ "10.0.1.2/24" ];
-    dns = [ "10.0.1.1" ];
-    mtu = 1412;
-    privateKeyFile = config.sops.secrets."misc/wireguard".path;
-
-    peers = [
-      {
-        publicKey = "wJMyQDXmZO4MjYRk6NK4+J6ZKWLTTZygAH+OwbPjOiw=";
-        allowedIPs = [
-          "10.0.1.0/24"
-          "104.16.0.0/12"
-          "172.64.0.0/13"
-        ];
-        endpoint = "${domain}:51820";
-        persistentKeepalive = 25;
-      }
-    ];
-  };
-
   environment.systemPackages = [ helper ];
+
+  networking.wg-quick.interfaces = {
+    ${wgIface} = {
+      autostart = false;
+      address = [ "10.0.1.2/24" ];
+      dns = [ "10.0.1.1" ];
+      mtu = 1412;
+      privateKeyFile = config.sops.secrets."misc/wireguard".path;
+
+      peers = [
+        {
+          publicKey = "wJMyQDXmZO4MjYRk6NK4+J6ZKWLTTZygAH+OwbPjOiw=";
+          allowedIPs = [
+            "10.0.1.0/24"
+            "104.16.0.0/12"
+            "172.64.0.0/13"
+          ];
+          endpoint = "${domain}:51820";
+          persistentKeepalive = 25;
+        }
+      ];
+    };
+
+    bud = {
+      autostart = true;
+      address = [ "10.54.132.2/24" ];
+      dns = [ "10.54.132.1" ];
+      mtu = 1420;
+      privateKeyFile = config.sops.secrets."misc/wireguard".path;
+
+      peers = [
+        {
+          publicKey = "O2GRMEWf22YRGKexHAdg1fitucTZ/U/om2MWEJMeyFQ=";
+          allowedIPs = [ "10.54.132.0/24" ];
+          endpoint = "dev.bud.studio:51820";
+          persistentKeepalive = 25;
+        }
+      ];
+    };
+  };
 }
