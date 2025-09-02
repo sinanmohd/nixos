166156387d84f2af81b185dfec6dc75c56708522
sinanmohd <sinan@sinanmohd.com>
2025-09-02 10:37:39 +0530

    chore(os/cez/wireguard/bud): update endpoint

--- os/cez/modules/wireguard.nix
+++ os/cez/modules/wireguard.nix
@@ -59,7 +59,7 @@ in
         {
           publicKey = "O2GRMEWf22YRGKexHAdg1fitucTZ/U/om2MWEJMeyFQ=";
           allowedIPs = [ "10.54.132.0/24" ];
-          endpoint = "dev.bud.studio:51820";
+          endpoint = "primary.k8s.bud.studio:51820";
           persistentKeepalive = 25;
         }
       ];
