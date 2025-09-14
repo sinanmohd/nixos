5fd8a7084e907f4c1b4c2c86cfd69f622b4c294a
sinanmohd <sinan@sinanmohd.com>
2025-09-14 19:24:26 +0530

    chore(pc/specialisation): make k3s a specialisation

--- os/pc/modules/work/default.nix
+++ os/pc/modules/work/default.nix
@@ -16,32 +16,34 @@ in
   virtualisation.docker.enable = true;
   users.extraGroups.docker.members = [ user ];
 
-  systemd.services.k3s.path = [ pkgs.criu ];
-  environment = {
-    variables.KUBECONFIG = "/etc/rancher/k3s/k3s.yaml";
-    systemPackages = with pkgs; [
-      kubernetes-helm
-      k9s
-    ];
-  };
-  services.k3s = {
-    enable = true;
-    gracefulNodeShutdown.enable = true;
-    clusterInit = true;
-    role = "server";
-    extraFlags = [
-      "--write-kubeconfig-group users"
-      "--write-kubeconfig-mode 0640"
-      # disabled because some wifi won't have IPv6 (2025 edition), and k3s fails on startup
-      # uncomment this to enble IPv6 ingress when humanity transcends
-      # "--cluster-cidr=10.42.0.0/16,fd12:b0d8:b00b::/56"
-      # "--service-cidr=10.43.0.0/16,fd12:b0d8:babe::/112"
-      # "--flannel-ipv6-masq"
-    ];
-    manifests.traefik-daemonset = {
+  specialisation.k3s.configuration = {
+    systemd.services.k3s.path = [ pkgs.criu ];
+    environment = {
+      variables.KUBECONFIG = "/etc/rancher/k3s/k3s.yaml";
+      systemPackages = with pkgs; [
+        kubernetes-helm
+        k9s
+      ];
+    };
+    services.k3s = {
       enable = true;
-      source = ./traefik-daemonset.yaml;
-      target = "traefik-daemonset.yaml";
+      gracefulNodeShutdown.enable = true;
+      clusterInit = true;
+      role = "server";
+      extraFlags = [
+        "--write-kubeconfig-group users"
+        "--write-kubeconfig-mode 0640"
+        # disabled because some wifi won't have IPv6 (2025 edition), and k3s fails on startup
+        # uncomment this to enble IPv6 ingress when humanity transcends
+        # "--cluster-cidr=10.42.0.0/16,fd12:b0d8:b00b::/56"
+        # "--service-cidr=10.43.0.0/16,fd12:b0d8:babe::/112"
+        # "--flannel-ipv6-masq"
+      ];
+      manifests.traefik-daemonset = {
+        enable = true;
+        source = ./traefik-daemonset.yaml;
+        target = "traefik-daemonset.yaml";
+      };
     };
   };
 }
