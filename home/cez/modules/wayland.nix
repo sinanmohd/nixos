4fff7e1fbcc216fd6344700da0ac75db45fecccc
sinanmohd <sinan@sinanmohd.com>
2025-10-22 09:01:05 +0530

    fix(home/cez/wayland): display positions

--- home/cez/modules/wayland.nix
+++ home/cez/modules/wayland.nix
@@ -3,6 +3,9 @@ let
   wayland-scripts = pkgs.callPackage ../../wayland/pkgs/wayland-scripts { };
   freezshot = "${wayland-scripts}/bin/freezshot";
   scale = 1.6;
+
+  laptop_screen_y = 2560;
+  monitor_position_y = (1 / scale) * laptop_screen_y;
 in
 {
   wayland.windowManager.sway.settings = {
@@ -12,11 +15,11 @@ in
     output = {
       "eDP-1" = {
         inherit scale;
-        position = "0,0";
+        position = "0 0";
       };
       "HDMI-A-1" = {
         inherit scale;
-        position = "2560,0";
+        position = "${builtins.toString (builtins.ceil monitor_position_y)} 0";
       };
     };
   };
