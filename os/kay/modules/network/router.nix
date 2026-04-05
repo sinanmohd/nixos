fd58498b3415f5a50cd08a8715f4bf216372fd43
sinanmohd <sinan@sinanmohd.com>
2026-04-05 22:43:09 +0530

    fix(os/kay/networkd): from tests

--- os/kay/modules/network/router.nix
+++ os/kay/modules/network/router.nix
@@ -1,12 +1,12 @@
 { ... }:
 let
-  # wanInterface = "ppp0";
+  wanInterface = "ppp0";
 
   gponInterface = "enp3s0";
   gponHost = "192.168.38.1";
   gponPrefix = 24;
 
-  lanInterface_1 = "enp8s0f3u1c2";
+  lanInterface_1 = "enp11s0f3u2c2";
   lanBridgeInterface = "lan";
   lanPrefix = 24;
   lanHost = "192.168.43.1";
@@ -29,7 +29,6 @@ in
       "30-${lanInterface_1}" = {
         matchConfig.Name = lanInterface_1;
         networkConfig.Bridge = lanBridgeInterface;
-        linkConfig.RequiredForOnline = "enslaved";
       };
       "40-${lanBridgeInterface}" = {
         matchConfig.Name = lanBridgeInterface;
@@ -37,9 +36,8 @@ in
         networkConfig = {
           Address = "${lanHost}/${toString lanPrefix}";
           DHCPServer = true;
-
-          IPMasquerade = "ipv4";
           IPv6Forwarding = true;
+          IPv4Forwarding = true;
         };
         dhcpServerConfig = {
           PoolOffset = 100;
@@ -54,27 +52,27 @@ in
     };
   };
   networking = {
-    # nat = {
-    #   enable = true;
-    #   externalInterface = wanInterface;
-    #   internalInterfaces = [ lanBridgeInterface ];
-    # };
+    nat = {
+      enable = true;
+      externalInterface = wanInterface;
+      internalInterfaces = [ lanBridgeInterface ];
+    };
     firewall = {
       allowedUDPPorts = [
         53
         67
       ];
       allowedTCPPorts = [ 53 ];
-      # extraCommands = ''
-      #   iptables -t mangle -A FORWARD -p tcp --tcp-flags SYN,RST SYN \
-      #       -o ${wanInterface} \
-      #       -j TCPMSS --clamp-mss-to-pmtu
-      # '';
-      # extraStopCommands = ''
-      #   iptables -t mangle -D FORWARD -p tcp --tcp-flags SYN,RST SYN \
-      #       -o ${wanInterface} \
-      #       -j TCPMSS --clamp-mss-to-pmtu
-      # '';
+      extraCommands = ''
+        iptables -t mangle -A FORWARD -p tcp --tcp-flags SYN,RST SYN \
+            -o ${wanInterface} \
+            -j TCPMSS --clamp-mss-to-pmtu
+      '';
+      extraStopCommands = ''
+        iptables -t mangle -D FORWARD -p tcp --tcp-flags SYN,RST SYN \
+            -o ${wanInterface} \
+            -j TCPMSS --clamp-mss-to-pmtu
+      '';
     };
   };
 
