1be30fd6acf8979e1e4c24d5efc2cb2a1ac28fa6
sinanmohd <sinan@sinanmohd.com>
2026-04-10 09:40:22 +0530

    chore(home/neovim): add lsp packages

--- nix/home/common/modules/neovim/default.nix
+++ nix/home/common/modules/neovim/default.nix
@@ -9,6 +9,19 @@
     gnumake
     # toggleterm
     tmux
+    # lsp
+    ccls
+    pyright
+    rust-analyzer
+    yaml-language-server
+    terraform-ls
+    bash-language-server
+    nil
+    tailwindcss-language-server
+    helm-ls
+    gopls
+    vue-language-server
+    luajitPackages.lua-lsp
   ];
 
   xdg.configFile.nvim.source = ./config;
